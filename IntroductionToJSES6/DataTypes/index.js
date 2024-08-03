42ee88d45db3102f39e6cde2f048ba40cfa8de62
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-03 06:01:05 +0200

    2024/08/03(am)
    JavaScript Data Types - Example Completed

---
+++ IntroductionToJSES6/DataTypes/index.js
@@ -0,0 +1,17 @@
+// // String
+// let string = "This is a String";
+
+// // Number
+// let number = 12345;
+
+// // Boolean
+// let boolean1 = true;
+// let boolean2 = false;
+
+alert(2+3);
+
+console.log(typeof(23));
+
+console.log(typeof("My Name"));
+
+console.log(typeof(true));
