81c2f571ed8bd737bd9bd375e0c54541a2a2f269
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-05 10:46:32 +0200

    2024/08/05(am)
    Naming and Naming Conventions for JavaScript Variables - Completed

---
+++ IntroductionToJSES6/NamingAndNamingConventions/index.js
@@ -0,0 +1,18 @@
+let myName = "Conrad";
+let yourName = "John Doe";
+
+
+// Can't declare variables with the name that is the same as a keyword
+// var var = "variable";
+
+// Can't declare variables starting with numbers:
+// var 1 = "variable 1";
+
+// Can't declare variables using spaces:
+// var an example = "no";
+
+// Variables can only contain letters, numbers, $ and _:
+// var abc123$_ = "example";
+
+// Variables in JavaScript are declared using camelCase:
+var variableExample = "Use camelCase;"
