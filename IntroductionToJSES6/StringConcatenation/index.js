4a6164a767ce712b2f45c46edfc8e9d420c900b8
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-05 10:54:17 +0200

    2024/08/05(am)
    JavaScript String Concatenation - Completed

---
+++ IntroductionToJSES6/StringConcatenation/index.js
@@ -0,0 +1,10 @@
+// let a = "a";
+// let b = "b";
+
+// console.log("Concatenating Strings: " + a + " " + b);
+
+let message = "Hello";
+
+let userName = prompt("What is your name?");
+
+alert(message + " " + userName);
