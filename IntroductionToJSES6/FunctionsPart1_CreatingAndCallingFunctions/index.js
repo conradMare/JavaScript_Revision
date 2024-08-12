baab73974ef982dc1b36e795652521bf1cb487e5
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-12 08:28:11 +0200

    2024/08/12(am)
    Functions Part 1: Creating and Calling Functions

--- IntroductionToJSES6/FunctionsPart1_CreatingAndCallingFunctions/index.js
+++ IntroductionToJSES6/FunctionsPart1_CreatingAndCallingFunctions/index.js
@@ -1,1 +1,15 @@
-alert("Hello World!");
+// For the Karel Robot:
+
+function getMilk() {
+    console.log("leaveHouse");
+    console.log("moveUp");
+    console.log("moveLeft");
+    console.log("moveUp");
+    console.log("buyMilk");
+    console.log("moveDown");
+    console.log("moveRight");
+    console.log("moveDown");
+    console.log("enterHouse");
+}
+
+getMilk();
