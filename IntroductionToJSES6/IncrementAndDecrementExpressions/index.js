6f975498c87160e683e7d75d420da774f18da9e6
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-12 07:50:04 +0200

    2024/08/12(am)
    Increment and Decrement Expressions - Completed

--- IntroductionToJSES6/IncrementAndDecrementExpressions/index.js
+++ IntroductionToJSES6/IncrementAndDecrementExpressions/index.js
@@ -1,1 +1,15 @@
-alert("Hello World!");
+// Increment
+// let x = 5;
+// x = x++; // 6
+
+// Decrement
+// let y = 6;
+// y = y--; // 5
+
+// Adding values
+let x = 5;
+let y = 3;
+x += y; //8
+
+// Subtracting Values
+x -= y; // 2
