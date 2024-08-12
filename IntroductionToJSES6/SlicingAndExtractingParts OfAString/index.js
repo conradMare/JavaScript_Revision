9abb8ae936f1fc33e609dfb879c5e28974f4d1cc
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-12 05:54:22 +0200

    2024/08/12(am)
    Slicing and Extracting Parts of a String

---
+++ IntroductionToJSES6/SlicingAndExtractingParts OfAString/index.js
@@ -0,0 +1,12 @@
+// let tweet = prompt("Compose your tweet: ");
+// alert("You have written " + tweet.length + " characters, you have " + (140 - tweet.length) + " characters remaining.");
+
+// let name = "Conrad";
+// console.log(name.slice(5, 6));
+
+// let tweet = prompt("Compose your tweet: ");
+// let tweetUnder140 = tweet.slice(0, 139);
+// alert(tweetUnder140);
+
+// Shortened 
+alert(prompt("Write a tweet:").slice(0, 140));
