82b0bf2f69428d7167002ac12c0df751da042f3f
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-13 06:56:20 +0200

    2024/08/13(am)
    BMI Calculator - Complete

--- JavaScriptExercises/BMICalculator/index.js
+++ JavaScriptExercises/BMICalculator/index.js
@@ -1,1 +1,11 @@
-alert("Hello World!");
+let weight = prompt("What is you weight?");
+let height = prompt("What is your height")
+
+function bmiCalculator(weight, height) {
+    let bmi = weight / (height * height);
+    return bmi;
+}
+
+let bmi = Math.floor(bmiCalculator(80, 1.79));
+
+alert("Your body mass index is " + bmi);
