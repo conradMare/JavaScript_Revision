afea7fcfb36f07ef57a57f114070962367010e6b
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-13 07:26:54 +0200

    2024/08/13(am)
    Random Love Calculator - Complete

--- JavaScriptExercises/RandomLoveCalculator/index.js
+++ JavaScriptExercises/RandomLoveCalculator/index.js
@@ -1,1 +1,9 @@
-alert("Hello World!");
+// LOVE CALCULATOR
+
+prompt("What is your name?");
+prompt("What is their name?");
+
+let loveScore = Math.random();
+loveScore = Math.floor(loveScore * 100) + 1;
+
+alert("Your love Score is " + loveScore + "%");
