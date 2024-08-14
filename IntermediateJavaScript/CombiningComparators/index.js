e2aa588e2ae1028b8c87c5e5a7ece7b49ab3ff8d
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-14 07:22:05 +0200

    2024/08/14(am)
    Combining Comparators - Complete

--- IntermediateJavaScript/CombiningComparators/index.js
+++ IntermediateJavaScript/CombiningComparators/index.js
@@ -1,1 +1,16 @@
-alert("Hello World!");
+prompt("What is your name?");
+prompt("What is their name?");
+
+let loveScore = Math.random();
+loveScore = Math.floor(loveScore * 100) + 1;
+
+// Combing comparators to get alerts for different result
+if (loveScore > 0 && loveScore <= 30) {
+    alert("Your love score is: " + loveScore + "%, You go together like oil and water.");
+} else if (loveScore > 30 && loveScore <= 50) {
+    alert("Your love score is: " + loveScore + "%, You go together like bees and badgers.");
+} else if (loveScore > 50 && loveScore <= 80) {
+    alert("Your love score is: " + loveScore + "%, You go together like Winnie the Pooh and Honey.");
+} else {
+    alert("Your love score is: " + loveScore + "%, You go together like cookies and cream.");
+}
