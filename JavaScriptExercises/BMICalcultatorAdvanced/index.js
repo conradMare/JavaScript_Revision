b5fb94e3d5d1ddb774ec0ed750fb3a18ce99cd57
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-14 07:48:04 +0200

    2024/08/14(am)
    BMI Calculator Advanced - Complete

--- JavaScriptExercises/BMICalcultatorAdvanced/index.js
+++ JavaScriptExercises/BMICalcultatorAdvanced/index.js
@@ -1,1 +1,18 @@
-alert("Hello World!");
+let weight = prompt("What is your weight?");
+let height = prompt("What is your height?");
+
+function bmiCalculator(weight, height) {
+
+    let bmi = Math.floor(weight / (height * height));
+    let interpretation;
+
+    if (bmi < 18.5) {
+        interpretation = "Your bmi is: " + bmi + ", you are under weight.";
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        interpretation = "Your bmi is: " + bmi + ", you have a normal weight.";
+    } else {
+        interpretation = "Your bmi is: " + bmi + ", You are overweight.";
+    }
+
+    return interpretation;
+}
