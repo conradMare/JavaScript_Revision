d3a1f7cc22e921cb49d6028d141e530396ed2f57
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-14 06:38:23 +0200

    2024/08/14(am)
    Control Statements: Using If-Else Conditionals & Logic - Complete

--- IntermediateJavaScript/ControlStatements_UsingIfElseConditionalsAndLogic/index.js
+++ IntermediateJavaScript/ControlStatements_UsingIfElseConditionalsAndLogic/index.js
@@ -1,1 +1,16 @@
-alert("Hello World!");
+prompt("What is your name?");
+prompt("What is their name?");
+
+let loveScore = Math.random();
+loveScore = Math.floor(loveScore * 100) + 1;
+
+if (loveScore >= 0 && loveScore < 30) {
+    alert("Your love score is: " + loveScore + "%" + " ,You go together like water and oil");
+} else if (loveScore >= 30 && loveScore < 50) {
+    alert("Your love score is: " + loveScore + "%" + " ,You go together like bees and badgers");
+} else if (loveScore >= 50 && loveScore < 80) {
+    alert("Your love score is: " + loveScore + "%" + " ,You go together like peanut butter and jelly");
+} else {
+    alert("Your love score is: " + loveScore + "%" + " ,You go together like Winnie the Pooh and Honey");
+}
+
