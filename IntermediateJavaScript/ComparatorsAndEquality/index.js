4abe5da5d8511c1778f26760dcb08c4a31abfe47
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-14 07:09:47 +0200

    2024/08/14(am)
    Comparators and Equality - Complete

--- IntermediateJavaScript/ComparatorsAndEquality/index.js
+++ IntermediateJavaScript/ComparatorsAndEquality/index.js
@@ -1,1 +1,13 @@
-alert("Hello World!");
+prompt("What is your name?");
+prompt("What is their name?");
+
+let loveScore = Math.random();
+loveScore = Math.floor(loveScore * 100) + 1;
+
+if (loveScore > 0 && loveScore <= 50) {
+    alert("Your love score is " + loveScore + "%, You go together like water and oil.");
+} else if (loveScore > 50 && loveScore <= 80) {
+    alert("Your love score is " + loveScore + "%, You go together like peanut butter and jelly.");
+} else {
+    alert("Your loveScore is " + loveScore + "%, You go together like Winnie the Pooh and Honey.");
+}
