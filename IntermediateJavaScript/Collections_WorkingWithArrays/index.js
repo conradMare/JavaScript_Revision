ece35df37143c0b80820c4b706e7dae9cb3dc995
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-14 08:33:47 +0200

    2024/08/14(am)
    Collections: Working with JS Arrays - Complete

--- IntermediateJavaScript/Collections_WorkingWithArrays/index.js
+++ IntermediateJavaScript/Collections_WorkingWithArrays/index.js
@@ -1,1 +1,9 @@
-alert("Hello World!");
+let guestList = ["Adam", "Ben", "Chuck", "David", "Jeff"];
+
+let guestName = prompt("What is your name, I'll check if you are on the list.");
+
+if (guestList.includes(guestName)) {
+    alert("Welcome!");
+} else {
+    alert("Sorry, your not on the guest list...");
+}
